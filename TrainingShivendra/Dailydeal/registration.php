f5168b5dec10750211bc498853091d5a7f4d05f0
shivendrakrish <79913941+shivendrakrish@users.noreply.github.com>
2021-03-30 08:55:20 +0530

    Delete TrainingShivendra directory
    Remove

--- TrainingShivendra/Dailydeal/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'TrainingShivendra_Dailydeal',
-    __DIR__
-);
--- TrainingShivendra/Insertdatathrowform/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'TrainingShivendra_Insertdatathrowform',
-    __DIR__
-);
