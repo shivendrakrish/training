1ba544967dbc1e1dbfdab5b1dbcd7211279e0ca7
shivendrakrish <79913941+shivendrakrish@users.noreply.github.com>
2021-03-30 09:19:47 +0530

    Add files via upload
    Custom Attribute

---
+++ TrainingShivendra/Customerattr/Helper/Data.php
@@ -0,0 +1,21 @@
+<?php
+namespace TrainingShivendra\Customerattr\Helper;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+    const XML_CONFIG_CUSTOMERATTR = 'customerattr/';
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue($field, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+    public function getGeneralConfig($code = null, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_CONFIG_CUSTOMERATTR .'general/'. $code, $storeId);
+    }
+
+}
